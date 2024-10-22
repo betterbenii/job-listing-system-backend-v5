ce52451c134be2a523b75bf8237cf48ca0e09fe0
betterbenii <abene983@gmail.com>
2024-10-22 21:02:28 +0300

    initialized the secret key in the env file and made sure the login request worked

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectToDatabase = require('./db');
+require('dotenv').config();
+const connectToDatabase = require('./db.js');
 const authRoutes = require('./routes/auth.js');
 const app = express();
 app.use(express.json());
